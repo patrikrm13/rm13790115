4943a9f56961d03d89df2fdd01501d7ef878ba61
patrikrm13 <mahdi028a@gmail.com>
2016-02-09 16:34:53 +0330

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,13 +1,13 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+639080623484", "AVVENGERRS", "AVENGERRS", ok_cb, false)
+send_contact(get_receiver(msg), "+639080623484", "AVENGERRS", "ANTI SPAM", ok_cb, false)
 end
 
 return {
 patterns = {
 "^!share$"
-"^شر$"
+
 },
 run = run
 }
