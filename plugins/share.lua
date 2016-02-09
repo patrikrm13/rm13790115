b912382449da8e8742b2bb2774a0880d2fef1dbe
patrikrm13 <mahdi028a@gmail.com>
2016-02-09 16:08:41 +0330

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+12817381338", "AVVENGERRS", "AVENGERRS", ok_cb, false)
+send_contact(get_receiver(msg), "+639080623484", "AVVENGERRS", "AVENGERRS", ok_cb, false)
 end
 
 return {
