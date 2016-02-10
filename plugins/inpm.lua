ab470fe751d9b8a8dc5b92eb96b7e09e862b4f60
patrikrm13 <mahdi028a@gmail.com>
2016-02-10 15:29:22 +0330

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -15,7 +15,7 @@ local function pairsByKeys (t, f)
 
 local function chat_list(msg)
     local data = load_data(_config.moderation.data)
-        local groups = 'groups'
+        local groups = 'گروه ها'
         if not data[tostring(groups)] then
                 return 'No groups at the moment'
         end
@@ -47,17 +47,17 @@ local function run(msg, matches)
       if is_gbanned(msg.from.id) then
             return 'You are globally banned.'
       end
-      if data[tostring(matches[2])]['settings']['lock_member'] == 'yes' and not is_owner2(msg.from.id, matches[2]) then
+      if data[tostring(matches[2])]['settings']['lock_member'] == 'فعال' and not is_owner2(msg.from.id, matches[2]) then
         return 'Group is private.'
       end
           local chat_id = "chat#id"..matches[2]
           local user_id = "user#id"..msg.from.id
    	  chat_add_user(chat_id, user_id, ok_cb, false)   
 	  local group_name = data[tostring(matches[2])]['settings']['set_name']	
-	  return "Added you to chat:\n\n👥"..group_name.." (ID:"..matches[2]..")"
+	  return "من شمارا به گروه دعوت کردم :):\n\n👥"..group_name.." (ID:"..matches[2]..")"
         elseif matches[1] == 'join' and not data[tostring(matches[2])] then
 		
-         	return "Chat not found."
+         	return "گروه ادد نشده"
         end
      if matches[1] == 'chats'then
        if is_admin(msg) and msg.to.type == 'chat' then
